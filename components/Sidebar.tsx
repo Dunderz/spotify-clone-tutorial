c181bb8e2df281a8032241a2b7980168166ffddf
Dunderz <isaacknam711@gmail.com>
2024-05-05 21:59:25 -0400

    button props

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation";
 import { useMemo } from "react";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import Library from "./Library";
 import Box from "./Box";
 import SidebarItem from "./SidebarItem";
 
@@ -34,8 +35,15 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
   return (
     <div className="flex h-full">
       <div
-        className="hidden md:flex flex-col gap-y-2 bg-black h-full
-       w-[300px] p2"
+        className="
+        hidden 
+        md:flex 
+        flex-col 
+        gap-y-2 
+        bg-black 
+        h-full
+        w-[300px] 
+        p-2"
       >
         <Box>
           <div
@@ -51,8 +59,11 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
             ))}
           </div>
         </Box>
-        <Box className="overflow-y-auto h-full">Song Library</Box>
+        <Box className="overflow-y-auto h-full">
+          <Library />
+        </Box>
       </div>
+      <main className="h-full flex-1 overflow-y-auto py-2">{children}</main>
     </div>
   );
 };
