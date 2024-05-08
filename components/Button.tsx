f3e56b63803c96e37a7d2bca81bbe92f597ae362
Dunderz <isaacknam711@gmail.com>
2024-05-07 23:41:27 -0400

    supabase

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,21 @@
 import { forwardRef } from "react";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * Props for the Button component.
+ */
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+/**
+ * A customizable button component.
+ * @param className - Additional CSS classes for the button.
+ * @param children - The content of the button.
+ * @param disabled - Whether the button is disabled.
+ * @param type - The type of the button.
+ * @param props - Additional props for the button.
+ * @param ref - Reference to the button element.
+ * @returns The rendered Button component.
+ */
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, children, disabled, type = "button", ...props }, ref) => {
@@ -10,7 +24,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
         type={type}
         className={twMerge(
           `w-full rounded-full bg-green-500 border border-transparent px-3
-      py-3 disabled:cursor-not-allowed disabled:opacity-50 text-black font-bold hover:opacity-75 transition`,
+          py-3 disabled:cursor-not-allowed disabled:opacity-50 text-black font-bold hover:opacity-75 transition`,
           className
         )}
       >
@@ -20,6 +34,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   }
 );
 
+// Set the display name for the Button component.
 Button.displayName = "Button";
 
 export default Button;
