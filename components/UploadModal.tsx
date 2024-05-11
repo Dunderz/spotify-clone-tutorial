3b31608b8b29e6aef8fe15e316250a54aa0fa55d
Dunderz <isaacknam711@gmail.com>
2024-05-10 21:26:12 -0400

    upload to supabase comment

---
+++ components/UploadModal.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { FieldValues, useForm } from "react-hook-form";
+
+import useUploadModal from "@/hooks/useUploadModal";
+import Modal from "./Modal";
+
+const UploadModal = () => {
+  const uploadModal = useUploadModal();
+
+  const { register, handleSubmit, reset } = useForm<FieldValues>({
+    defaultValues: {
+      title: "",
+      author: "",
+      song: null,
+      image: null,
+    },
+  });
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+      reset();
+      uploadModal.onClose();
+    }
+  };
+
+  const onSubmit = () => {
+    // Upload to supabase
+  };
+
+  return (
+    <Modal
+      title="Add a song"
+      description="Upload an mp3 file"
+      isOpen={uploadModal.isOpen}
+      onChange={onChange}
+    >
+      Form
+    </Modal>
+  );
+};
+
+export default UploadModal;
