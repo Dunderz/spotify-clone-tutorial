259ba159f699541c43e67558a93d928d66e0799b
Dunderz <isaacknam711@gmail.com>
2024-05-17 13:49:53 -0400

    Account page

---
+++ app/account/components/AccountContent.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+const AccountContent = () => {
+  const router = useRouter();
+  return <div></div>;
+};
+
+export default AccountContent;
